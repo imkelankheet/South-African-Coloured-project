7d0fb74be20da7a589005e3694d234600145cbef
imkelankheet <83457194+imkelankheet@users.noreply.github.com>
2024-05-14 10:26:51 +0200

    Update phasing_prep2.sh

--- phasing_prep2.sh
+++ phasing_prep2.sh
@@ -17,6 +17,7 @@ module load SHAPEIT/v2.r837
 cd /mosaic_working_directory/1.unphased
 echo "Working on prep2 for chromosome $chr"
 
+# Running shapeit to remove those variants that were identified as those that needed to be removed in the previous step
 shapeit -check --thread 8 \
     --input-ref ${KGP_Phase3}/1000GP_Phase3_chr${chr}.hap.gz ${KGP_Phase3}/1000GP_Phase3_chr${chr}.legend.gz ${KGP_Phase3}/1000GP_Phase3.sample \
     -B bed_file_coloured_chr{chr} \
